072437589e9733ba1a2016ed2a7175392d3c3fb7
K <home@Kevin.local>
2014-08-16 17:44:28 -0500

    first commit

---
+++ Assets/Scripts/GeneradorItem.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GeneradorItem : MonoBehaviour {
+
+	public GameObject[] obj;
+	public float tiempoMin = 1f;
+	public float tiempoMax = 3f;
+
+	// Use this for initialization
+	void Start () {
+		GenerarItem ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	void GenerarItem(){
+		Instantiate (obj [Random.Range (0, obj.Length)], transform.position, Quaternion.identity);
+		Invoke("GenerarItem", Random.Range(tiempoMin, tiempoMax));
+	}
+}
---
+++ Assets/Scripts/GeneradorPiso.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GeneradorPiso : MonoBehaviour {
+
+	public GameObject piso;
+	public float tiempoMin = 5f;
+
+	// Use this for initialization
+	void Start () {
+				
+		NotificationCenter.DefaultCenter ().AddObserver (this, "PersonajeEmpezoACorrer");
+	}
+
+	void PersonajeEmpezoACorrer(Notification notification){
+		Generar ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	void Generar(){
+		Instantiate (piso, transform.position, Quaternion.identity);
+		Invoke ("Generar", tiempoMin);
+	}
+}
