55aeee94194fc04048a0b3c5d09c02cbd64bfe45
Kevin <rojke34@gmail.com>
2014-08-16 20:36:38 -0500

    add new component

---
+++ Assets/Scripts/Item.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Item : MonoBehaviour {
+
+	private bool haColisionadoConElJugador = false;
+	public int puntosGanados = 1;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	void OnTriggerEnter2D(Collider2D colision){
+		if (!haColisionadoConElJugador && colision.gameObject.tag == "Player") {
+			haColisionadoConElJugador = true;
+			Destroy(gameObject);
+			NotificationCenter.DefaultCenter().PostNotification(this, "IncrementarPuntos", puntosGanados);
+		}
+	}
+}
---
+++ Assets/Scripts/Puntuacion.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Puntuacion : MonoBehaviour {
+
+	private int puntuacion = 0;
+
+	// Use this for initialization
+	void Start () {
+		NotificationCenter.DefaultCenter ().AddObserver (this, "IncrementarPuntos");
+	}
+
+	void IncrementarPuntos(Notification notificacion){
+		int puntosAincrementar = (int)notificacion.data;
+		puntuacion += puntosAincrementar;
+		Debug.Log ("Incrementeo: " + puntosAincrementar + " Puntos. Total ganados: " + puntuacion);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
